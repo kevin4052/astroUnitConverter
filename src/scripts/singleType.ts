efe1b71704411858d19befee431677db7f84a64a
kevin4052 <kevin.hernandez001@gmail.com>
2025-03-01 15:31:18 -0600

    single conversion page

---
+++ src/scripts/singleType.ts
@@ -0,0 +1,55 @@
+import unitData from "@data/unitData.ts"
+
+// get elements from the DOM
+const input = document.getElementById("input") as HTMLInputElement;
+const selectTo = document.getElementById("selectTo") as HTMLSelectElement;
+const selectFrom = document.getElementById(
+        "selectFrom"
+    ) as HTMLSelectElement;
+
+// get selected unit type value
+const getSelectFrom = (): string => {
+    const selectFrom = document.getElementById(
+        "selectFrom"
+    ) as HTMLSelectElement;
+    return selectFrom.value;
+};
+
+const getSelectTo = (): string => {
+    const selectTo = document.getElementById("selectTo") as HTMLSelectElement;
+    return selectTo.value;
+};
+
+
+// calculate the conversion
+const cal = () => {
+    const input = document.getElementById("input") as HTMLInputElement;
+    const output = document.getElementById("output") as HTMLInputElement;
+    const title = document.getElementById("title") as HTMLElement;
+    const unitType = title.innerText.toLocaleLowerCase();
+    const inputValue = input.value;
+
+    const selectFrom = getSelectFrom();
+    const selectTo = getSelectTo();
+
+    if (inputValue === "" || !Number(inputValue)) {
+        output.value = "";
+        return;
+    }
+
+    const fromConvertion =
+        unitData[unitType][
+            selectFrom as keyof typeof unitData
+        ];
+    const toConvertion =
+        unitData[unitType][
+            selectTo as keyof typeof unitData
+        ];
+
+    output.value = (Number(inputValue) * (fromConvertion[1] / toConvertion[1])).toString();
+};
+
+// Event listeners
+input.addEventListener("keyup", () => cal());
+selectFrom.addEventListener("change", () => cal());
+selectTo.addEventListener("change", () => cal());
